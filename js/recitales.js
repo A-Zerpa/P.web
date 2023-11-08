ef2203d27fd08952da9b1d8410a187ed7e594023
Nicole Mata <143561344+nicoleMata@users.noreply.github.com>
2023-11-08 00:32:32 -0300

    agregue el calendario

--- js/recitales.js
+++ js/recitales.js
@@ -9,3 +9,27 @@ $('.slider').slick({
     variableWidth: true,
     variableHeight: true,
 });
+
+// calendario
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendario');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['dayGrid'],
+        locale: 'es',
+        events: [
+            {
+                title: 'Curso 1 Javascript',
+                start: '2023-12-16',
+                end: '2023-12-30'
+            },
+            {
+                title: 'Curso 2',
+                start: '2023-06-20',
+                end: '2023-07-05'
+            }
+        ]
+    });
+
+    calendar.render();
+});
